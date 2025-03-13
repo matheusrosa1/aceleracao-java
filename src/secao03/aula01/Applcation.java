9f61d241c9e99121bf9900616728b1bde281101c
Matheus Rosa <matheusrosataxa@gmail.com>
2025-03-12 21:24:06 -0300

    feat: secao03-aula1

---
+++ src/secao03/aula01/Applcation.java
@@ -0,0 +1,7 @@
+package secao03.aula01;
+
+public class Applcation {
+    public static void main(String[] args) {
+        Television television = new Television("Philips", "LCD", 32);
+    }
+}
---
+++ src/secao03/aula01/Television.java
@@ -0,0 +1,14 @@
+package secao03.aula01;
+
+public class Television {
+    boolean isOn = false;
+    String brand;
+    String model;
+    int size;
+
+    public Television(String brand, String model, int size) {
+        this.brand = brand;
+        this.model = model;
+        this.size = size;
+    }
+}
